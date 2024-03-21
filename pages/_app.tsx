a76546bbc9873ca9775401299106ce468f7a39b9
Shivendra TechSter <shivendratechSter@gmail.com>
2024-03-21 23:31:24 +0530

    add daisyui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import "@/styles/globals.css";
+import Navbar from "@/components/navbar";
+("@/components/navbar");
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <div className="container mx-auto p-4">
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
