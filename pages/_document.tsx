5d2f80bad9c4b8bbce6334059c717f94330bcd6d
Shivendra TechSter <shivendratechSter@gmail.com>
2024-04-01 05:29:51 +0530

    theme based on user system theme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,20 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { useEffect, useState } from "react";
 
 export default function Document() {
+  
+  useEffect(() => {
+    const prefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+
+    setTheme(prefersDark ? "dark" : "light");
+  }, []);
+
+  const [theme, setTheme] = useState("");
+
   return (
-    <Html lang="en" data-theme="light">
+    <Html lang="en" data-theme={theme}>
       <Head />
       <body>
         <Main />
