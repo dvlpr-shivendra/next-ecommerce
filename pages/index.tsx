551881808f5a0bce33248bdf0f18b00afb8052e4
Shivendra TechSter <shivendratechSter@gmail.com>
2024-04-11 21:45:27 +0530

    Homepage - improve responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
 import ProductCard from "@/components/product-card";
-import { format } from "@/helpers/currency";
 import { get } from "@/helpers/http";
-import { thumbnail } from "@/helpers/product";
 import { backendUrl } from "@/helpers/urls";
-import Link from "next/link";
 
 type GetProductsRes = {
   products: Product[];
@@ -13,7 +10,7 @@ export default function Home({ data }: { data: GetProductsRes }) {
   return (
     <div className="mt-14">
       <h1 className="text-4xl font-bold mb-14">Ecommerce</h1>
-      <div className="max-w-max mx-auto grid grid-cols-1 lg:grid-cols-4 gap-16 justify-items-center">
+      <div className="mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-16 justify-items-center">
         {data.products.map((product) => (
           <ProductCard key={`product-card-${product.id}`} product={product} />
         ))}
@@ -23,9 +20,8 @@ export default function Home({ data }: { data: GetProductsRes }) {
 }
 
 export async function getServerSideProps() {
-  // Fetch data from external API
+  
   const data: GetProductsRes = await get(backendUrl("products"), false);
 
-  // Pass data to the page via props
   return { props: { data } };
 }
