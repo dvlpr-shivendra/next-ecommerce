fc024ed63a485b46fcd0ebe55067ab6870e75aff
Shivendra TechSter <shivendratechSter@gmail.com>
2024-04-11 20:57:43 +0530

    Extract product card

---
+++ components/product-card.tsx
@@ -0,0 +1,31 @@
+import { format } from "@/helpers/currency";
+import { thumbnail } from "@/helpers/product";
+import Link from "next/link";
+import React from "react";
+
+function ProductCard({ product }: { product: Product }) {
+  return (
+    <Link
+      href={`/products/${product.id}`}
+      key={`product-card-${product.id}`}
+      className="card w-64 bg-base-100 shadow-xl"
+      title={"Buy - " + product.title}
+    >
+      <img
+        className="mx-auto h-48 w-48 bg-white object-contain"
+        src={thumbnail(product)}
+        alt="Shoes"
+      />
+      <div className="p-6 text-center">
+        <h2 className="line-clamp-1 mb-4" title={product.title}>
+          {product.title}
+        </h2>
+        <p className="leading-none text-green-500 font-bold mb-4">
+          {format(product.price)}
+        </p>
+      </div>
+    </Link>
+  );
+}
+
+export default ProductCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import ProductCard from "@/components/product-card";
 import { format } from "@/helpers/currency";
 import { get } from "@/helpers/http";
 import { thumbnail } from "@/helpers/product";
@@ -14,26 +15,7 @@ export default function Home({ data }: { data: GetProductsRes }) {
       <h1 className="text-4xl font-bold mb-14">Ecommerce</h1>
       <div className="max-w-max mx-auto grid grid-cols-1 lg:grid-cols-4 gap-16 justify-items-center">
         {data.products.map((product) => (
-          <Link
-            href={`/products/${product.id}`}
-            key={`product-card-${product.id}`}
-            className="card w-64 bg-base-100 shadow-xl"
-            title={"Buy - " + product.title}
-          >
-            <img
-              className="mx-auto h-48 w-48 bg-white object-contain"
-              src={thumbnail(product)}
-              alt="Shoes"
-            />
-            <div className="p-6 text-center">
-              <h2 className="line-clamp-1 mb-4" title={product.title}>
-                {product.title}
-              </h2>
-              <p className="leading-none text-green-500 font-bold mb-4">
-                {format(product.price)}
-              </p>
-            </div>
-          </Link>
+          <ProductCard key={`product-card-${product.id}`} product={product} />
         ))}
       </div>
     </div>
