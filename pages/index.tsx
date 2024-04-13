743f5af11679e39aa43ccec84c428cef4570b78d
Shivendra TechSter <shivendratechSter@gmail.com>
2024-04-13 14:37:05 +0530

    Hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,13 @@ type GetProductsRes = {
 export default function Home({ data }: { data: GetProductsRes }) {
   return (
     <div className="mt-14">
-      <h1 className="text-4xl font-bold mb-14">Ecommerce</h1>
+      <div className="mt-8 mb-12 p-6 border border-accent border-dashed">
+        <h1 className="text-3xl font-bold leading-none text-gray-900 mb-4">
+          SimplyBuy
+        </h1>
+        <p className="text-lg">Streamlined shopping, simplified for you.</p>
+      </div>
+
       <div className="mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 justify-items-center">
         {data.products.map((product) => (
           <ProductCard key={`product-card-${product.id}`} product={product} />
@@ -20,7 +26,6 @@ export default function Home({ data }: { data: GetProductsRes }) {
 }
 
 export async function getServerSideProps() {
-  
   const data: GetProductsRes = await get(backendUrl("products"), false);
 
   return { props: { data } };
