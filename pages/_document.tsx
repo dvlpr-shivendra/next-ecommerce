0cb6ef0631e100a5e791181f80fdfa5f6697e74c
Shivendra TechSter <shivendratechSter@gmail.com>
2024-04-14 22:49:44 +0530

    use montserrat

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,6 @@ import { Html, Head, Main, NextScript } from "next/document";
 import { useEffect, useState } from "react";
 
 export default function Document() {
-  
   useEffect(() => {
     const prefersDark = window.matchMedia(
       "(prefers-color-scheme: dark)"
@@ -14,9 +13,20 @@ export default function Document() {
   const [theme, setTheme] = useState("");
 
   return (
-    <Html lang="en" data-theme={'lofi'}>
-      <Head />
-      <body>
+    <Html lang="en" data-theme={"lofi"}>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body className="font-[Montserrat]">
         <Main />
         <NextScript />
       </body>
